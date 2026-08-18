7100bf068fc9937248a72229a2518ec6a1e7390c
GruberWilfried <wilfried.gruber@htl-salzburg.ac.at>
2026-08-18 23:57:52 +0200

    Rework Screen 1_Foyer

--- Assets/Sina/Scripts/StringAbgleicher.cs
+++ Assets/Sina/Scripts/StringAbgleicher.cs
@@ -6,12 +6,19 @@ public class StringAbgleicher : MonoBehaviour
     [SerializeField] private InputField inputField;
     [SerializeField] private string correctWord;
 
+    public Animator anim;
+
     public void InputSent()
     {
         if (inputField.text == correctWord)
         {
             RoomManager roomManager = FindFirstObjectByType<RoomManager>();
             roomManager.RoomCompleted(1);
+            anim.gameObject.SetActive(false);
+        }
+        else
+        {
+            anim.Play("wrongbirthday");
         }
     }
 }
